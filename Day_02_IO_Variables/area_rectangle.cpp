65ec09d905e29616d08014c0f002e58cbb3fa29b
Rachana <rachanamehta09@gmail.com>
2025-07-05 12:37:28 +0530

    Day 2: Variables, Input/Output, and Practice Problems

---
+++ Day_02_IO_Variables/area_rectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the side :"<<endl;
+    cin>>a>>b;
+    cout<<"Area of rectangle :"<<a*b<<endl;
+
+    return 0;
+}
